2b46c2ad56a4c56ca93a6093681d266da991ef6f
Your Name <you@example.com>
2022-05-04 00:44:50 +0545

    Complete to do list

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,10 +2,13 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food"];
+const items = ["Buy Food", "Cook Food"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -13,36 +16,49 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-  var today = new Date();
-
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  var day = today.toLocaleDateString("en-US", options);
-
+const day =  date.getDay()
 
   res.render("list", {
-    kinfOfDay: day,
+    listTitle: day,
     newListItems: items
   });
 
 });
 
 app.post("/", function(req, res) {
-  var item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
 });
 
-// app.post("/", function(req, res) {
-//
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.get("/about", function(req,res){
+  res.render("about");
+})
+
+// app.post("/work", function(req, res) {
+//   const item = req.body.newItem;
+//   workItems.push(item);
+//   res.redirect("/work");
 // });
 
 app.listen(3000, function() {
