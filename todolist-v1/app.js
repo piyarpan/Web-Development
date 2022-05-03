e1330d02e3b32485a43a7ce9973706ebfe06adfa
Your Name <you@example.com>
2022-05-03 12:11:06 +0545

    Add todo list

---
+++ todolist-v1/app.js
@@ -0,0 +1,50 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items = ["Buy Food", "Cook Food"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+
+  var today = new Date();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+
+  res.render("list", {
+    kinfOfDay: day,
+    newListItems: items
+  });
+
+});
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
+
+// app.post("/", function(req, res) {
+//
+// });
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
